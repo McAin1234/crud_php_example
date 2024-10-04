5a5f9c3bed150bd42a0b2945b0529ac03bc09dac
Rafael Romero Espejo <romero.espejo.rafael@iescamas.es>
2024-10-04 10:01:49 +0200

    se ha cambiao la bbdd

---
+++ bbdd/libros.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE libreria;
+
+USE libreria;
+CREATE TABLE libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    autor VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL
+);
